10c8b0f4ee4ca128d450c04f799a9701331e9799
ana204 <anagomes.lucia@hotmail.com>
2021-06-02 17:50:29 -0300

    activity_loanding

--- app/src/main/java/com/eletronico/caixa_eletronico/loading.java
+++ app/src/main/java/com/eletronico/caixa_eletronico/loading.java
@@ -2,7 +2,9 @@ package com.eletronico.caixa_eletronico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class loading extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class loading extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //chamada da função
+                ActivityPassword();
+            }
+        },3000);
+    }
+
+    //função para fazer a navegação entre activity
+    private void ActivityPassword()
+    {
+        Intent intent = new Intent(loading.this, password.class);
+        startActivity(intent);
+        finish();
     }
 }
