87aa38f0db5e37977a8bcaf70514418cc4186b24
ana204 <anagomes.lucia@hotmail.com>
2021-06-04 16:03:18 -0300

    Confirm password and bank balance screen

--- app/src/main/java/com/eletronico/caixa_eletronico/password.java
+++ app/src/main/java/com/eletronico/caixa_eletronico/password.java
@@ -2,13 +2,48 @@ package com.eletronico.caixa_eletronico;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class password extends AppCompatActivity {
+
+    private EditText password;
+    private Button buttonConfirmar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+
+        password = findViewById(R.id.editTextPassword);
+        buttonConfirmar = findViewById(R.id.buttonConfirmar);
+
+        buttonConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int passwordCadastrada = 123456;
+                int passwordAtual;
+
+                passwordAtual = Integer.parseInt(password.getText().toString());
+
+                if(passwordAtual == passwordCadastrada)
+                {
+                    ActivityMain();
+                } else {
+                    Toast.makeText(password.this, "Senha inválida, tente novamente", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void ActivityMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
